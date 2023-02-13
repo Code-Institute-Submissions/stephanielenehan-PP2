dcd57e4437c182ffddfd6ed2e74d80f7026ad3f2
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2023-02-13 11:31:32 +0000

    add basic javaScript set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("Connected")
+var me; 
+var myScore = 0; 
+var computer;
+var computerScore = 0;
+
+var chooseFrom = ["rock", "paper", "scissors"]; 
+
+window.onload = function() {
+    for (let i = 0; i < 3; i++) {
+
+        let choice = document.createElement("img"); 
+        choice.id = choice[i]; 
+        choices.src = choices[i] + ".jpeg"; 
+        document.getElementById("choices").append(choice);
+    }
+}
