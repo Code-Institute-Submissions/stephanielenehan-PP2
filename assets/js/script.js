232ab1d0acb787461ff6fae06a601b83eb924134
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2023-02-13 12:36:58 +0000

    add select choice function in JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,27 @@ var myScore = 0;
 var computer;
 var computerScore = 0;
 
-var chooseFrom = ["rock", "paper", "scissors"]; 
+var chooseFrom = ["rock"; "paper"; "scissors"]; 
 
 window.onload = function() {
     for (let i = 0; i < 3; i++) {
+        // <img id="rock" src="assets/images/rockicon.jpeg">
 
         let choice = document.createElement("img"); 
         choice.id = choice[i]; 
-        choices.src = choices[i] + ".jpeg"; 
+        choice.src = choices[i] + ".jpeg"; 
+        choice.addEventListener("click", selectChoice); 
         document.getElementById("choices").append(choice);
     }
+}
+
+function selectChoice() {
+    you = this.id;
+    document.getElementById("my-choice").src = you + ".jpeg"; 
+
+    computer = choices[Math.floor(Math.random() * 3 )];
+    document.getElementById("computer-choice").src = computer + ".jpeg";
+
+
+
 }
