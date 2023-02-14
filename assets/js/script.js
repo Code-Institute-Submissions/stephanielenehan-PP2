56c00883145f799af335b4313285827bc290d56e
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2023-02-14 11:38:22 +0000

    correct scissors image from jpg to png

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var myScore = 0;
 var computer;
 var computerScore = 0;
 
-var choices = ["rock", "paper", "scissors"]; 
+var choices = ["rock", "spock", "scissors"]; 
 
 window.onload = function() {
     for (let i = 0; i < 3; i++) {
@@ -11,7 +11,7 @@ window.onload = function() {
 
         let choice = document.createElement("img"); 
         choice.id = choice[i]; 
-        choice.src = choices[i] +  "assets/images/choices/.png"; 
+        choice.src = choices[i] + "assets/images/choices/.png"; 
         choice.addEventListener("click", selectChoice); 
         document.getElementById("choices").append(choice);
     }
@@ -19,7 +19,7 @@ window.onload = function() {
 
 function selectChoice() {
     you = this.id;
-    document.getElementById("my-choice").src = you + "assets/images/choices/.png"; 
+    document.getElementById("my-choice").src = me + "assets/images/choices/.png"; 
 
     computer = choices[Math.floor(Math.random() * 3 )];
     document.getElementById("computer-choice").src = computer + "assets/images/choices/.png";
