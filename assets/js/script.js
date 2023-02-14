e2afeb0f5374fa5a1eed3327cef3b9c2533079ce
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2023-02-14 12:36:08 +0000

    relocate images outside folders to test if issue source or not

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,26 @@ var myScore = 0;
 var computer;
 var computerScore = 0;
 
-var choices = ["rock", "spock", "scissors"]; 
+var choices = ["rock", "paper", "scissors"]; 
 
 window.onload = function() {
     for (let i = 0; i < 3; i++) {
         
 
         let choice = document.createElement("img"); 
-        choice.id = choice[i]; 
-        choice.src = choices[i] + "assets/images/choices/.png"; 
+        choice.id = choices[i]; 
+        choice.src = choices[i] + ".png"; 
         choice.addEventListener("click", selectChoice); 
         document.getElementById("choices").append(choice);
     }
 }
 
 function selectChoice() {
-    you = this.id;
-    document.getElementById("my-choice").src = me + "assets/images/choices/.png"; 
+    me = this.id;
+    document.getElementById("my-choice").src = me + ".png"; 
 
     computer = choices[Math.floor(Math.random() * 3 )];
-    document.getElementById("computer-choice").src = computer + "assets/images/choices/.png";
+    document.getElementById("computer-choice").src = computer + ".png";
 
 
 
