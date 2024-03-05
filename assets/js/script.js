d8ae991174cb97719bf57154d1585690d33a26fd
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2024-03-05 12:13:54 +0000

    comment out old JS to write own custom code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,31 @@
+//Commented out old code from YT tutorial as out of date and some bad practices//
 //Code based on you tube tutorial by Kenny Yip Coding. Link in ReadMe.
-let me; 
+//let me; 
 
-let computer;
+//let computer;
 
 
-let choices = ["rock", "paper", "scissors"]; 
+//let choices = ["rock", "paper", "scissors"]; 
 
-window.onload = function() {
-    for (let i = 0; i < 3; i++) {
+//window.onload = function() {
+    //for (let i = 0; i < 3; i++) {
         
 
-        let choice = document.createElement("img"); 
-        choice.id = choices[i]; 
-        choice.src = `assets/images/${choices[i]}.jpg`;
-        choice.addEventListener("click", selectChoice); 
-        document.getElementById("choices").append(choice);
-    }
-}
+        //let choice = document.createElement("img"); 
+        //choice.id = choices[i]; 
+        //choice.src = `assets/images/${choices[i]}.jpg`;
+        //choice.addEventListener("click", selectChoice); 
+        //document.getElementById("choices").append(choice);
+    //}
+//}
 
-function selectChoice() {
-    me = this.id;
-    document.getElementById("my-choice").src = `assets/images/${me}.jpg`;
+//function selectChoice() {
+    //me = this.id;
+    //document.getElementById("my-choice").src = `assets/images/${me}.jpg`;
 
-    computer = choices[Math.floor(Math.random() * 3 )];
-    document.getElementById("computer-choice").src = `assets/images/${computer}.jpg`;
+    //computer = choices[Math.floor(Math.random() * 3 )];
+    //document.getElementById("computer-choice").src = `assets/images/${computer}.jpg`;
 
 
 
-}
+//}
